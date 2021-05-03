1d6b3415149b84232993a4813ce4cd65e19afd85
Lin Yang <github@linyang.me>
2021-05-02 23:35:00 -0700

    Make it safter to include tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-set -o errexit
 set -o nounset
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-ln -fs $ROOT_DIR/tmux.conf $HOME/.tmux.conf
+if [ -f "$HOME/.tmux.conf" ]; then
+  grep "source-file $ROOT_DIR/tmux.conf" $HOME/.tmux.conf &> /dev/null
+  if [ $? == 0 ]; then
+    echo "Already included $ROOT_DIR/tmux.conf in $HOME/.tmux.conf"
+  else
+    echo "Including $ROOT_DIR/tmux.conf in $HOME/.tmux.conf"
+    echo "source-file $ROOT_DIR/tmux.conf" >> $HOME/.tmux.conf
+  fi
+else
+  echo "Creating $HOME/.tmux.conf and including $ROOT_DIR/tmux.conf in it"
+  touch $HOME/.tmux.conf
+  echo "source-file $ROOT_DIR/tmux.conf" >> $HOME/.tmux.conf
+fi
